4af28d827e929998bf45c146e4b4c39c67583712
Caxvalencia <caxvalencia@gmail.com>
2021-10-23 15:46:34 -0500

    feat(ui-angular): add button component to ui

--- libs/ui/angular/src/lib/components/ui-angular-components.module.ts
+++ libs/ui/angular/src/lib/components/ui-angular-components.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { BreadcrumbComponent } from './breadcrumb/breadcrumb.component';
+import { ButtonComponent } from './button/button.component';
 import { SearcherComponent } from './searcher/searcher.component';
 
-const DECLARATIONS = [SearcherComponent, BreadcrumbComponent];
+const DECLARATIONS = [SearcherComponent, BreadcrumbComponent, ButtonComponent];
 
 @NgModule({
   imports: [CommonModule, FormsModule, ReactiveFormsModule],
